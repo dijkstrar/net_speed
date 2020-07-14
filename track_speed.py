603c43cbefda7580fa60e9a325d9c760f48ba076
= <=>
2020-07-14 22:36:28 +0200

    chromium driver support

--- track_speed.py
+++ track_speed.py
@@ -1,4 +1,4 @@
-from selenium.webdriver import Firefox, FirefoxOptions
+from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,16 +7,16 @@ from bs4 import BeautifulSoup
 from contextlib import contextmanager
 
 @contextmanager
-def get_firefox() -> Firefox:
+def get_chrome() -> Chrome:
     # https://docs.python.org/3.7/library/contextlib.html#contextlib.contextmanager
-    opts = FirefoxOptions()
+    opts = ChromeOptions()
     opts.headless = True
-    driver = Firefox(options=opts,executable_path=r'geckodriver.exe')
+    driver = Chrome(options=opts,executable_path=r'/usr/lib/chromium-browser/chromedriver')
     yield driver
     driver.close()
 
 
-def wait_visible(driver: Firefox, selector: str, timeout: int = 2):
+def wait_visible(driver: Chrome, selector: str, timeout: int = 2):
     cond = EC.visibility_of_any_elements_located((By.CSS_SELECTOR, selector))
     try:
         WebDriverWait(driver, timeout).until(cond)
@@ -39,21 +39,24 @@ def extract_speed_info(soup: BeautifulSoup) -> dict:
 
 
 def run_speed_test() -> dict:
-    with get_firefox() as driver:
+    with get_chrome() as driver:
+        print('visit page')
         driver.get('https://fast.com/en')
         
         # Obtain download speeds
         download_done_selector = '#speed-value.succeeded'
         wait_visible(driver, download_done_selector, timeout=60)
-        
+        print('VISIBLE')
         # Obtain upload speed
         link = driver.find_element_by_link_text('Show more info')
         link.click()
+        print('CLICKED')
         upload_done_selector = '#upload-value.succeeded'
         wait_visible(driver, upload_done_selector, timeout=60)
         
         
         # Select the resulting speeds
+        print('SELECT RESULTS')
         results_selector = '.speed-controls-container'
         results_el = driver.find_element_by_css_selector(results_selector)
         results_html = results_el.get_attribute('outerHTML')
